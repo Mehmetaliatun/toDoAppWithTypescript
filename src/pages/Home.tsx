04cdcdeaf0775e4c3eb50414e442d100e0cbf6fa
Atun <mali1996ua@gmail.com>
2022-11-30 20:40:28 +0300

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,27 @@ import InputForm from "../components/InputForm";
 import TodoList from "../components/TodoList";
 
 const url = "https://63878fced9b24b1be3f4431c.mockapi.io/todos";
+interface TodoType {
+  id: string | number;
+  task: string;
+  isDone: boolean;
+}
 
 const Home = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
 
-  interface TodoType {
-    id: string | number;
-    task: string;
-    isDone: boolean;
-  }
+  const getTodos = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return (
     <div className="main">
