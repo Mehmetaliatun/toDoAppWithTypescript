f212522f32ebfc98aaf32a155001070972355bdc
Atun <mali1996ua@gmail.com>
2022-11-30 20:27:37 +0300

    url, useState

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,25 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import InputForm from '../components/InputForm'
-import TodoList from '../components/TodoList';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import InputForm from "../components/InputForm";
+import TodoList from "../components/TodoList";
 
+const url = "https://63878fced9b24b1be3f4431c.mockapi.io/todos";
 
 const Home = () => {
-  
-  
+  const [todos, setTodos] = useState([]);
+
+  interface TodoType {
+    id: string | number;
+    task: string;
+    isDone: boolean;
+  }
 
   return (
-    <div className='main'>
-      <InputForm  />
+    <div className="main">
+      <InputForm />
       <TodoList />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
